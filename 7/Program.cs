e33cab034ebf66f78a5086b2b7a4b257e31052b7
widerhin <widerhin@yandex.ru>
2023-06-10 16:34:36 +0300

    task n7

---
+++ 7/Program.cs
@@ -0,0 +1,5 @@
+﻿// последняя цифра числа
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastNum = number%10;
+Console.WriteLine($"{lastNum} последняя цифра");
