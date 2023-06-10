b1c1f5934ff13d2f6d4f2b847c9623a401d1c782
widerhin <widerhin@yandex.ru>
2023-06-10 14:43:06 +0300

    task n1

---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// является ли 1-ое число квадратом второго
+Console.WriteLine("введите первое число");
+int quart = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (quart == number * number)
+{
+    Console.WriteLine($"число {quart} является квадратом числа {number}");
+}
+else
+{
+    Console.WriteLine($"число {quart} не является квадратом числа {number}");
+}
