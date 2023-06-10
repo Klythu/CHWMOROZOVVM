97257e60a05bf25bcc88adcc394eb894b39aa066
widerhin <widerhin@yandex.ru>
2023-06-10 17:03:31 +0300

    task n4

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,2 +1,14 @@
-﻿Console.WriteLine("введите 3 числа через запятую");
-
+﻿Console.WriteLine("введите поочередно 3 числа ");
+int[] nums= new int[3];
+nums[0] = Convert.ToInt32(Console.ReadLine());
+nums[1] = Convert.ToInt32(Console.ReadLine());
+nums[2] = Convert.ToInt32(Console.ReadLine());
+int max = nums[0];
+foreach (int s in nums)
+{
+    if(max<s)
+    {
+        max=s;
+    }
+}
+Console.WriteLine($"{max} наибольшее число");
