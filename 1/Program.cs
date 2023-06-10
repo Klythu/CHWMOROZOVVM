2702a1e94e357aae498968507662eccc549f515f
widerhin <widerhin@yandex.ru>
2023-06-10 14:09:48 +0300

    first task

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите 1-ое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите 2-ое число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 < number2)
+{
+    Console.Write("максимальное число ");
+    Console.Write(number2);
+}
+else
+{
+    Console.Write("максимальное число ");
+    Console.Write(number1);
+}
