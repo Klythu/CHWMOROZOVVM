680fbab1142b544d5dd8a9369eb8257cfacf360b
widerhin <widerhin@yandex.ru>
2023-06-10 16:26:15 +0300

    task n5

---
+++ 5/Program.cs
@@ -0,0 +1,14 @@
+﻿//числа от -n до n
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] nums= new int[number*2+1];
+int i = 0;
+number= -number;
+while (i < nums.Length)
+{
+    nums[i]=nums[i]+number;
+    number = number+1;
+    i =i+1;
+}
+foreach (int s in nums) Console.Write($"{s} ");
+
