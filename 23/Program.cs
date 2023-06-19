cdf817a98d0eb923f2b5713793b073e626da86aa
widerhin <widerhin@yandex.ru>
2023-06-19 15:04:26 +0300

    task 23

---
+++ 23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("введите число");
+int num= Convert.ToInt32(Console.ReadLine());
+int[] nums = new int[num];
+int i = 0;
+while (i<nums.Length)
+{
+    nums[i]=nums[i]+(1+i)*(1+i)*(1+i);
+    i++;
+}
+foreach (int s in nums)
+{
+    Console.WriteLine(s);
+}
