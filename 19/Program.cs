3324445fa521a4ca3086baa5edf196e82c5a49d1
widerhin <widerhin@yandex.ru>
2023-06-19 14:35:49 +0300

    task 19

---
+++ 19/Program.cs
@@ -0,0 +1,39 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+string Callstr()
+{
+    Console.WriteLine("введите число из 5 цифр");
+
+    string number= Console.ReadLine();
+    return number;
+}
+string num= string.Empty;
+
+while (num.Length!=5)
+{
+    num=Callstr();
+}
+bool polin=false;
+int i=0;
+while (num[i]== num[num.Length-1-i])
+{
+    if (i==num.Length/2-1)
+    {
+        
+        polin=true;
+        break;
+    }
+   i++;
+}
+
+if (polin)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+
+
