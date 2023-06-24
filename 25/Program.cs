5a77310bb882882891bfd4928e1876a39fb646de
widerhin <widerhin@yandex.ru>
2023-06-24 19:57:17 +0300

    task 29

--- 25/Program.cs
+++ 25/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-Console.WriteLine("введите первое число");
+Console.WriteLine("введите основание степени");
 int number= Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите второе число");
+Console.WriteLine("введите показатель степени");
 int power= Convert.ToInt32(Console.ReadLine());
 int i=1;
 int res =number;
---
+++ 29/Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+int InputI(string msg)
+{
+    Console.WriteLine(msg);
+    int inp=int.Parse(Console.ReadLine());
+    return inp;
+}
+
+int Irendo(int min,int max)
+{
+    if (min>max)
+    {
+        Random rand= new Random();
+        int res=rand.Next(max,min);
+        return res;
+    }
+    else
+    {
+        Random rand= new Random();
+        int res=rand.Next(min,max);
+        return res;
+    }
+    
+}
+int[] GenArr(int len,int min, int max )
+{
+    int[] arr= new int[len];
+    int i=0;
+    while (i<len)
+    {
+        
+        
+        arr[i]+=Irendo(min,max);
+        i++;
+    }
+    return arr;
+}
+void ArrSee(int[] arr)
+{
+    Console.Write("[");
+    int i=0;
+    while (i<arr.Length-1)
+    {
+        Console.Write($"{arr[i]},");
+        i++;
+    }
+    Console.Write(arr[i]);
+    Console.Write("]");
+}
+string nnn="ggg";
+int volum=InputI("длинна масива");
+int downnum=InputI("нижняя граница разброса");
+int upnum=InputI("верхняя граница разброса");
+int[] des= GenArr(volum,downnum,upnum);
+ArrSee(des);
