ea31100a7d92b8006fbe50bd1f6597f48c6de523
widerhin <widerhin@yandex.ru>
2023-06-24 17:30:19 +0300

    task 25

---
+++ 25/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("введите первое число");
+int number= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int power= Convert.ToInt32(Console.ReadLine());
+int i=1;
+int res =number;
+while (i<power)
+{
+    i++;
+    res=res*number;
+}
+Console.WriteLine(res);
---
+++ test/Program.cs
@@ -0,0 +1,38 @@
+﻿double[] ReadArr3(string input)
+{
+    double[] array= new double[3];
+    string[] arr= new string[array.Length];
+    int i=0;
+    int j=0;
+    while (i<array.Length)
+   {
+        if (input[j].Equals("."))
+        {
+            i++;
+            j++;
+        }
+        if (input[j].Equals(" "))
+        {
+            j++;
+        }
+        arr[i]=arr[i]+input[j];
+        j++;
+        if (j>=input.Length-1)
+        {
+            break;
+        }
+    }
+    i=0;
+    while(i<array.Length)
+    {
+        array[i]=Convert.ToDouble(arr[i]);
+        i++;
+    }
+    return array;
+}
+Console.WriteLine("введите координаты первого вектора");
+double[] arr1= new double[3];
+arr1=ReadArr3(Console.ReadLine());
+Console.WriteLine(arr1[0]+1);
+Console.WriteLine(arr1[1]+1);
+Console.WriteLine(arr1[2]+1);
